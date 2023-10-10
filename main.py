aee7abd8c8fe9a04ed676375332e9b696278db21
Danyil Zlobin <alice@programmingmentor.com>
2023-10-10 19:28:50 +0300

    Threads_HW2 Updates v. 2.0

--- main.py
+++ main.py
@@ -1,51 +1,60 @@
-import queue
 import threading
+import time
 
 
-def collatz_steps(n, lock):
-    with lock:
-        steps = 0
-        while n > 1:
-            if n % 2 == 0:
-                n //= 2
-            else:
-                n = 3 * n + 1
-            steps += 1
-        global total_steps
-        total_steps += steps
+# Function to calculate the Collatz step for a given number
+def collatz_func(num):
+    new_num = 0
+    while num != 1:
+        if num % 2 == 0:
+            num = num // 2
+        else:
+            num = 3 * num + 1
+        new_num += 1
+    return new_num
 
 
-# Функція для обчислення кількості кроків для чисел з черги
-def process_numbers(q, lock):
-    for _ in range(q.qsize()):
-        try:
-            number = q.get(block=False)
-            collatz_steps(number, lock)
-            q.task_done()
-        except queue.Empty:
-            break
+def main(n, num_threads):
+    start_time = time.time()
+
+    # List of numbers to calculate gradients for
+    numbers = list(range(1, n + 1))
+
+    threads = []  # List to hold thread objects
+    results = []  # List to store results from each thread
+    locks = [threading.Lock() for _ in range(num_threads)]  # Create locks for each thread
+
+    def worker(nums, thread_id):
+        thread_results = []
+        for num in nums:
+            thread_results.append(collatz_func(num))
+        # Acquire the lock before modifying the shared 'results' list
+        with locks[thread_id]:
+            results.extend(thread_results)
+
+    for i in range(num_threads):
+        # Divide the work among threads
+        start_idx = i * (n // num_threads)
+        end_idx = (i + 1) * (n // num_threads)
+        thread_numbers = numbers[start_idx:end_idx]
+
+        # Create a thread for each range of numbers
+        thread = threading.Thread(target=worker, args=(thread_numbers, i))
+        threads.append(thread)
+        thread.start()
+
+    # Wait for all threads to complete
+    for thread in threads:
+        thread.join()
+
+    # Calculate the mean gradient from the results
+    mean_gradient = sum(results) / len(results)
+
+    print("Середня кількість кроків:", mean_gradient)
+    print("Час виконання:", time.time() - start_time)
 
 
-num_threads = 4
-
-N = 1000
-numbers_queue = queue.Queue()
-for i in range(1, N + 1):
-    numbers_queue.put(i)
-
-total_steps = 0
-lock = threading.Lock()
-
-threads = []
-for _ in range(num_threads):
-    thread = threading.Thread(target=process_numbers, args=(numbers_queue, lock))
-    threads.append(thread)
-    thread.start()
-
-for thread in threads:
-    thread.join()
-
-average_steps = total_steps / N
-
-print(f"Середня кількість кроків для виродження в 1: {average_steps}")
-
+if __name__ == "main":
+    n_nums = 1000
+    num_threads = 10
+    main(n_nums, num_threads)
